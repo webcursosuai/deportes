b150f426257d2c39c75c95a69b7cda58b672c25f
Sikeze <mipozarski@alumnos.uai.cl>
2017-07-21 17:03:22 -0400

    access control

--- modules.php
+++ modules.php
@@ -41,136 +41,141 @@ $editid = optional_param("editid", null, PARAM_INT);
 
 $context = context_system::instance();
 
-$url = new moodle_url("/local/deportes/modules.php");
-$PAGE->navbar->add(get_string("nav_title", "local_deportes"));
-$PAGE->navbar->add(get_string("modules", "local_deportes"), $url);
-$PAGE->set_context($context);
-$PAGE->set_url($url);
-$PAGE->set_pagelayout("standard");
-$PAGE->set_title(get_string("page_title", "local_deportes"));
-$PAGE->set_heading(get_string("page_heading", "local_deportes"));
-
-if($action == 'add'){
-	$moduleform = new sports_addmodule_form();
+if(($email[1] == $CFG->deportes_emailextension) || is_siteadmin() || has_capability("local/deportes:edit", $context)){
 	
-	if ($moduleform->is_cancelled()) {
-		$backtolist = new moodle_url('/local/deportes/modules.php', array(
-				'action' => 'view'));
-		redirect($backtolist);
+	$url = new moodle_url("/local/deportes/modules.php");
+	$PAGE->navbar->add(get_string("nav_title", "local_deportes"));
+	$PAGE->navbar->add(get_string("modules", "local_deportes"), $url);
+	$PAGE->set_context($context);
+	$PAGE->set_url($url);
+	$PAGE->set_pagelayout("standard");
+	$PAGE->set_title(get_string("page_title", "local_deportes"));
+	$PAGE->set_heading(get_string("page_heading", "local_deportes"));
+	
+	if($action == 'add'){
+		$moduleform = new sports_addmodule_form();
 		
-	} elseif ($data = $moduleform->get_data()) {
-		// Saves the form info in to variables.
-		$module = new stdClass();
-		$module->name = $data->name;
-		$module->starttime = $data->starttime;
-		$module->endtime = $data->endtime;
-		$module->type = $data->type;
-		$module->timecreated = time();
-		$module->timemodified = time();
-		
-		if($DB->insert_record('sports_modules', $module)){
-			$status = 'module inserted';
-		}
-		$backtolist = new moodle_url('/local/deportes/modules.php', array(
-				'action' => 'view'));
-		redirect($backtolist);
-	}
-}
-
-if($action == 'view'){
-	if($modules = $DB->get_records('sports_modules')){
-		$status = 'existen registros';
-		$table = new html_table();
-		$table->head = array(
-				get_string('name','local_deportes'),
-				get_string('initialtime','local_deportes'),
-				get_string('endtime','local_deportes'),
-				get_string('type','local_deportes'),
-				get_string('edit','local_deportes'),
-				get_string('delete','local_deportes')
-		);
-		foreach($modules as $module){
-			$type = ($module->type == 0) ? 'Outdoor' : 'Fitness';
-			//Add a button for each sport for editing or deleting
-			$urledit = new moodle_url("/local/deportes/modules.php", array(
-					"action" => "edit",
-					"editid" => $module->id,
-			));
-			$editmoduleicon = new pix_icon("i/edit", "Editar");
-			$urldelete = new moodle_url("/local/deportes/modules.php", array(
-					"action" => "delete",
-					"editid" => $module->id,
-			));
-			$deletemoduleicon = new pix_icon("t/delete", "Borrar");
-			$table->data[] = array(
-					$module->name,
-					$module->starttime,
-					$module->endtime,
-					$type,
-					$OUTPUT->action_icon($urledit, $editmoduleicon),
-					$OUTPUT->action_icon($urldelete, $deletemoduleicon, new confirm_action(get_string('delete_confirmation','local_deportes')))
-			);
-		}
-	}
-}
-if($action == 'edit'){
-	if($module = $DB->get_record('sports_modules',array("id"=>$editid))){
-		$editform = new sports_editmodule_form(null, array(
-				"id"=>$module->id
-		));
-		$editform->set_data($module);
-		if ($editform->is_cancelled()){
-			$action = "view";
-			redirect($url);
-		}elseif($data = $editform->get_data()){
+		if ($moduleform->is_cancelled()) {
+			$backtolist = new moodle_url('/local/deportes/modules.php', array(
+					'action' => 'view'));
+			redirect($backtolist);
+			
+		} elseif ($data = $moduleform->get_data()) {
+			// Saves the form info in to variables.
 			$module = new stdClass();
-			$module->id = $data->editid;
 			$module->name = $data->name;
 			$module->starttime = $data->starttime;
 			$module->endtime = $data->endtime;
 			$module->type = $data->type;
+			$module->timecreated = time();
 			$module->timemodified = time();
-			if($DB->update_record('sports_modules', $module)){
-				
+			
+			if($DB->insert_record('sports_modules', $module)){
+				$status = 'module inserted';
 			}
 			$backtolist = new moodle_url('/local/deportes/modules.php', array(
 					'action' => 'view'));
 			redirect($backtolist);
 		}
-		
-	}else{
-		$backtolist = new moodle_url('/local/deportes/modules.php', array(
-				'action' => 'view'));
-		redirect($backtolist);
 	}
-}
-if($action == 'delete'){
-	if($editid != null){
-		$DB->delete_records('sports_modules', array("id"=>$editid));
-		$backtolist = new moodle_url('/local/deportes/modules.php', array(
-				'action' => 'view'));
-		redirect($backtolist);
-	}else{
-		$backtolist = new moodle_url('/local/deportes/modules.php', array(
-				'action' => 'view'));
-		redirect($backtolist);
+	
+	if($action == 'view'){
+		if($modules = $DB->get_records('sports_modules')){
+			$status = 'existen registros';
+			$table = new html_table();
+			$table->head = array(
+					get_string('name','local_deportes'),
+					get_string('initialtime','local_deportes'),
+					get_string('endtime','local_deportes'),
+					get_string('type','local_deportes'),
+					get_string('edit','local_deportes'),
+					get_string('delete','local_deportes')
+			);
+			foreach($modules as $module){
+				$type = ($module->type == 0) ? 'Outdoor' : 'Fitness';
+				//Add a button for each sport for editing or deleting
+				$urledit = new moodle_url("/local/deportes/modules.php", array(
+						"action" => "edit",
+						"editid" => $module->id,
+				));
+				$editmoduleicon = new pix_icon("i/edit", "Editar");
+				$urldelete = new moodle_url("/local/deportes/modules.php", array(
+						"action" => "delete",
+						"editid" => $module->id,
+				));
+				$deletemoduleicon = new pix_icon("t/delete", "Borrar");
+				$table->data[] = array(
+						$module->name,
+						$module->starttime,
+						$module->endtime,
+						$type,
+						$OUTPUT->action_icon($urledit, $editmoduleicon),
+						$OUTPUT->action_icon($urldelete, $deletemoduleicon, new confirm_action(get_string('delete_confirmation','local_deportes')))
+				);
+			}
+		}
 	}
-}
-
-echo $OUTPUT->header();
-echo $OUTPUT->heading("DeportesUAI");
-
-if($action == 'add'){
-	$moduleform->display();
-}
-if($action == 'view'){
-	if($status != null){
-		echo html_writer::table($table);
+	if($action == 'edit'){
+		if($module = $DB->get_record('sports_modules',array("id"=>$editid))){
+			$editform = new sports_editmodule_form(null, array(
+					"id"=>$module->id
+			));
+			$editform->set_data($module);
+			if ($editform->is_cancelled()){
+				$action = "view";
+				redirect($url);
+			}elseif($data = $editform->get_data()){
+				$module = new stdClass();
+				$module->id = $data->editid;
+				$module->name = $data->name;
+				$module->starttime = $data->starttime;
+				$module->endtime = $data->endtime;
+				$module->type = $data->type;
+				$module->timemodified = time();
+				if($DB->update_record('sports_modules', $module)){
+					
+				}
+				$backtolist = new moodle_url('/local/deportes/modules.php', array(
+						'action' => 'view'));
+				redirect($backtolist);
+			}
+			
+		}else{
+			$backtolist = new moodle_url('/local/deportes/modules.php', array(
+					'action' => 'view'));
+			redirect($backtolist);
+		}
 	}
-	$botonurl = new moodle_url('/local/deportes/modules.php', array('action' => 'add'));
-	echo $OUTPUT->single_button($botonurl,get_string('addmodule','local_deportes'));
+	if($action == 'delete'){
+		if($editid != null){
+			$DB->delete_records('sports_modules', array("id"=>$editid));
+			$backtolist = new moodle_url('/local/deportes/modules.php', array(
+					'action' => 'view'));
+			redirect($backtolist);
+		}else{
+			$backtolist = new moodle_url('/local/deportes/modules.php', array(
+					'action' => 'view'));
+			redirect($backtolist);
+		}
+	}
+	
+	echo $OUTPUT->header();
+	echo $OUTPUT->heading("DeportesUAI");
+	
+	if($action == 'add'){
+		$moduleform->display();
+	}
+	if($action == 'view'){
+		if($status != null){
+			echo html_writer::table($table);
+		}
+		$botonurl = new moodle_url('/local/deportes/modules.php', array('action' => 'add'));
+		echo $OUTPUT->single_button($botonurl,get_string('addmodule','local_deportes'));
+	}
+	if($action == 'edit'){
+		$editform->display();
+	}
+	echo $OUTPUT->footer();
+}else{
+	print_error(get_string("notallowed", "local_deportes"));
 }
-if($action == 'edit'){
-	$editform->display();
-}
-echo $OUTPUT->footer();
--- reserve.php
+++ reserve.php
@@ -36,77 +36,82 @@ if (isguestuser()) {
 
 $context = context_system::instance();
 
-$url = new moodle_url("/local/deportes/reserve.php");
-$PAGE->navbar->add(get_string("nav_title", "local_deportes"));
-$PAGE->navbar->add(get_string("reserve", "local_deportes"), $url);
-$PAGE->set_context($context);
-$PAGE->set_url($url);
-$PAGE->set_pagelayout("standard");
-$PAGE->set_title(get_string("page_title", "local_deportes"));
-$PAGE->set_heading(get_string("page_heading", "local_deportes"));
-
-$email = $USER->email;
-if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
-	print_error(get_string("notvalidemail", "local_deportes"));
-}
-
-$curl = curl_init();
-$url = $CFG->deportes_urldeportesalumno;
-$token = $CFG->sync_token;
-$fields = array(
-		"token" => $token,
-		"email" => $email
-);
-curl_setopt($curl, CURLOPT_URL, $url);
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
-curl_setopt($curl, CURLOPT_POST, TRUE);
-curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($fields));
-curl_setopt($curl, CURLOPT_HTTPHEADER, array("Content-Type: application/json"));
-$result = json_decode(curl_exec($curl));
-curl_close($curl);
-
-$table = new html_table("p");
-
-$table->head = array(
-		get_string("sport", "local_deportes"),
-		get_string("teacher", "local_deportes"),
-		get_string("t_start", "local_deportes"),
-		get_string("t_end", "local_deportes"),
-		get_string("reserved", "local_deportes"),
-		get_string("quota", "local_deportes"),
-		""
-);
-
-$table->size = array(
-		"15%",
-		"20%",
-		"15%",
-		"15%",
-		"15%",
-		"10%",
-		"10%"
-);
-
-foreach($result as $sport){
-	$sportinfo = array(
-			$sport->name,
-			$sport->led_by,
-			$sport->whenHHMM,
-			$sport->endHHMM,
-			$sport->reservados,
-			$sport->capacity,
-			$OUTPUT->single_button('https://intranet.uai.cl/WebPages/Deporte/Reservas.aspx',get_string('reserve','local_deportes'))
+if(($email[1] == $CFG->deportes_emailextension) || is_siteadmin() || has_capability("local/deportes:edit", $context)){
+	$url = new moodle_url("/local/deportes/reserve.php");
+	$PAGE->navbar->add(get_string("nav_title", "local_deportes"));
+	$PAGE->navbar->add(get_string("reserve", "local_deportes"), $url);
+	$PAGE->set_context($context);
+	$PAGE->set_url($url);
+	$PAGE->set_pagelayout("standard");
+	$PAGE->set_title(get_string("page_title", "local_deportes"));
+	$PAGE->set_heading(get_string("page_heading", "local_deportes"));
+	
+	$email = $USER->email;
+	if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+		print_error(get_string("notvalidemail", "local_deportes"));
+	}
+	
+	$curl = curl_init();
+	$url = $CFG->deportes_urldeportesalumno;
+	$token = $CFG->sync_token;
+	$fields = array(
+			"token" => $token,
+			"email" => $email
+	);
+	curl_setopt($curl, CURLOPT_URL, $url);
+	curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+	curl_setopt($curl, CURLOPT_POST, TRUE);
+	curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($fields));
+	curl_setopt($curl, CURLOPT_HTTPHEADER, array("Content-Type: application/json"));
+	$result = json_decode(curl_exec($curl));
+	curl_close($curl);
+	
+	$table = new html_table("p");
+	
+	$table->head = array(
+			get_string("sport", "local_deportes"),
+			get_string("teacher", "local_deportes"),
+			get_string("t_start", "local_deportes"),
+			get_string("t_end", "local_deportes"),
+			get_string("reserved", "local_deportes"),
+			get_string("quota", "local_deportes"),
+			""
 	);
 	
-	$table->data[] = $sportinfo	;
-}
-
-echo $OUTPUT->header();
-echo $OUTPUT->heading("DeportesUAI");
-echo $OUTPUT->tabtree(deportes_tabs(), "reserve");
-if(empty($result)){
-	echo html_writer::div(get_string("nosportsleft","local_deportes"),"alert alert-info", array("role"=>"alert"));
+	$table->size = array(
+			"15%",
+			"20%",
+			"15%",
+			"15%",
+			"15%",
+			"10%",
+			"10%"
+	);
+	
+	foreach($result as $sport){
+		$sportinfo = array(
+				$sport->name,
+				$sport->led_by,
+				$sport->whenHHMM,
+				$sport->endHHMM,
+				$sport->reservados,
+				$sport->capacity,
+				$OUTPUT->single_button('https://intranet.uai.cl/WebPages/Deporte/Reservas.aspx',get_string('reserve','local_deportes'))
+		);
+		
+		$table->data[] = $sportinfo	;
+	}
+	
+	echo $OUTPUT->header();
+	echo $OUTPUT->heading("DeportesUAI");
+	echo $OUTPUT->tabtree(deportes_tabs(), "reserve");
+	if(empty($result)){
+		echo html_writer::div(get_string("nosportsleft","local_deportes"),"alert alert-info", array("role"=>"alert"));
+	}else{
+		echo html_writer::table($table);
+	}
+	echo $OUTPUT->footer();
 }else{
-	echo html_writer::table($table);
+	print_error(get_string("notallowed", "local_deportes"));
 }
-echo $OUTPUT->footer();
+}
